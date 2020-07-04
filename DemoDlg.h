b0e64d471b2fdf7639729a50dede5d86791a92d6
Snmoflower <1464890555@qq.com>
2020-07-04 11:54:06 +0800

    添加项目文件。

---
+++ DemoDlg.h
@@ -0,0 +1,44 @@
+﻿
+// DemoDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CDemoDlg 对话框
+class CDemoDlg : public CDialogEx
+{
+// 构造
+public:
+	CDemoDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DEMO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	
+	CString m_sWritePath;
+	char* Str1 = "hello world";
+	char* Str2 = "hello 交大";
+	int Count1 = strlen("hello world");
+	int Count2 = strlen("hello 交大");
+	afx_msg void OnClickedSearch();
+	afx_msg void OnClickedWrite1();
+	afx_msg void OnClickedWrite2();
+};
